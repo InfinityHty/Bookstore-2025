e580cadb71f6bdd4ae31fe657a39a913cc57f493
InfinityHty <infinityhty.sjtu.edu.cn>
2025-12-26 23:59:15 +0800

    if cnt = 0 cout nothing.

--- src/main.cpp
+++ src/main.cpp
@@ -234,7 +234,7 @@ int main() {
                         else {
                             int cnt = log.ComputeCount(tokens[2]);
                             if (cnt < 0 || cnt > log.trade_cnt) valid = false;
-                            else if (cnt == 0) std::cout << "\n";
+                            //else if (cnt == 0) std::cout << "\n";
                             else {
                                 log.ShowFinance(cnt);
                                 log.AddRecord(cur_user,"ask to show financial record");
